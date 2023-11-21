6b6e6bab71733fb8e28a0172a5244c148d5cd067
manzoorWayar <softwareengineerwayar@gmail.com>
2023-11-22 04:18:36 +0430

    s

--- src/app/api/customers/route.js
+++ src/app/api/customers/route.js
@@ -1,12 +1,12 @@
 
-import { Customer } from "@/models";
+import { News } from "@/models";
 import { NextResponse } from "next/server";
 import connectDB from "../../lib/connectDB";
 
 
 connectDB();
 export async function GET(request) {
-  const customers = await Customer.find({})
+  const customers = await News.find({})
 
   return NextResponse.json(customers);
 }
