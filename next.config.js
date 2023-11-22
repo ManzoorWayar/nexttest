efb0f6f99f545d133446385eb4757465da5744a0
manzoorWayar <softwareengineerwayar@gmail.com>
2023-11-22 05:32:58 +0430

    m

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     env: {
         APP_ENV: "development",
         // MONGO_URI: "mongodb://127.0.0.1:27017/tech",
-        MONGO_URI: "mongodb+srv://news:newsWebsiteNext.js@cluster0.ba23ry6.mongodb.net/",
+        MONGO_URI: "mongodb+srv://mitservice:Ouv3LK9u7hs0j6xb@cluster0.rskulnd.mongodb.net/",
         FORONT_URL: "http://localhost:3000",
     },
     experimental: {
